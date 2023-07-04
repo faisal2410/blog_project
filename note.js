c352c9180d0879704418d0f979d9dd9cb3234c49
Faisal ahmed <faisal2410@yahoo.com>
2023-07-04 12:36:37 +0600

    updated

---
+++ note.js
@@ -0,0 +1,16 @@
+// 👉Steps for study the repository :
+
+// Project Setup
+// Installing Laravel Breeze
+// Defining Migrations
+// Defining Models/Relationships
+// Creating Models, Controllers, Factories & Migrations
+// Inserting Data Using Factories & Seeders
+// Setting up Article Routes
+// Setting up The Frontend Pages
+// The index() method
+// The show () method
+// The create() & store() Methods
+// The edit() & Update Method
+// The destroy() method
+// Using Policies for Unauthorized users
